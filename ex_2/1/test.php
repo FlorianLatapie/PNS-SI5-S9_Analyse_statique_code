d416f7955774d1dab9357a6c824a54224bd73eda
Florian Latapie <florian.latapie@etu.unice.fr>
2024-02-10 09:37:46 -0500

    Update test.php

--- ex_2/1/test.php
+++ ex_2/1/test.php
@@ -9,7 +9,7 @@ libxml_disable_entity_loader (false);
 $xml = strlen($_GET['xml']) > 0 ? $_GET['xml'] : '<root><content>No XML found</content></root>';
 
 $document = new DOMDocument();
-$document->loadXML($xml, LIBXML_DTDLOAD); # https://www.php.net/manual/en/libxml.constants.php : LIBXML_NOENT Caution Enabling entity substitution may facilitate XML External Entity (XXE) attacks.
+$document->loadXML($xml, LIBXML_NONET); # https://www.php.net/manual/en/libxml.constants.php : LIBXML_NOENT Caution Enabling entity substitution may facilitate XML External Entity (XXE) attacks.
 $parsedDocument = simplexml_import_dom($document);
 
 echo $parsedDocument->content;
