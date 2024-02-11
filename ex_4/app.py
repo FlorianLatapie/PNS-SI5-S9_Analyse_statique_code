9fbd1ca6a3da41f8a9199c4147b9668145c22d33
Florian Latapie <florian.latapie@etu.unice.fr>
2024-02-11 17:02:51 +0100

    Update app.py

--- ex_4/app.py
+++ ex_4/app.py
@@ -4,13 +4,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     name = request.args.get('name') 
-    template = f'''
-        <h1>Welcome {name}!</h1>
-    '''
-    return render_template_string(template)
+    template = f"<h1>Welcome {name}!</h1>"
+    return template
 
 if __name__ == '__main__':
    app.run(debug=True)
 
 # flask run 
-# http://127.0.0.1:5000/?name=%3Cscript%3Ealert(%22xss%22)%3C/script%3E
+# http://127.0.0.1:5000/?name=%3Cscript%3Ealert(%22XSS%20hehehehehe%22);%3C/script%3E
